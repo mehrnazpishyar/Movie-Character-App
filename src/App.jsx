17fa3ae6f66b14696a0ab449eb36c83e98f754c4
mehrnaz <pishyar.mehrnaz@gmail.com>
2023-12-17 17:37:36 +0100

    add character detail component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-
+import { useState } from "react";
+import { allCharacters } from "../data/data";
 import "./App.css";
 import CharacterDetail from "./components/CharacterDetail";
 import CharacterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [characters, setCharacters] = useState(allCharacters);
 
   return (
     <div className="app">
-      <Navbar />
-      <div className="main">
-        <CharacterList />
-        <CharacterDetail  />
-      </div>
+      <Navbar numOfResult={characters.length} />
+      <Main > 
+      <CharacterList characters={characters} />
+      <CharacterDetail />
+      </Main>
     </div>
   );
 }
 
 export default App;
 
+function Main({ children }) {
+  return (
+    <div className="main">
+    {children}
+    </div>
+  );
+}
--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,72 +1,46 @@
 import { EyeIcon } from "@heroicons/react/24/outline";
 
-const allCharacters = [
-    {
-      id: 1,
-      name: "Rick Sanchez",
-      status: "Dead",
-      species: "Human",
-      type: "",
-      gender: "Male",
-      origin: {
-        name: "Earth (C-137)",
-        url: "https://rickandmortyapi.com/api/location/1",
-      },
-      location: {
-        name: "Citadel of Ricks",
-        url: "https://rickandmortyapi.com/api/location/3",
-      },
-      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-      created: "2017-11-04T18:48:46.250Z",
-    },
-    {
-      id: 2,
-      name: "Summer Smith",
-      status: "Alive",
-      species: "Human",
-      type: "",
-      gender: "Female",
-      origin: {
-        name: "Earth (Replacement Dimension)",
-        url: "https://rickandmortyapi.com/api/location/20",
-      },
-      location: {
-        name: "Earth (Replacement Dimension)",
-        url: "https://rickandmortyapi.com/api/location/20",
-      },
-      image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-      created: "2017-11-04T19:09:56.428Z",
-    },
-  ];
 
-function CharacterList() {
+function CharacterList({characters}) {
   return (
     <div className="character-list">
-      {allCharacters.map((item) => (
-            <Character key={item.id} item={item} />
-          ))}
-      {console.log(allCharacters)}
+      {characters.map((item) => (
+        <Character key={item.id} item={item} />
+      ))}
     </div>
   );
 }
 
 export default CharacterList;
 
+function Character({ item }) {
+  return (
+    <div className="list__item">
+      <img src={item.image} alt={item.name} />
+      <CharacterName item={item} />
+      <CharacterInfo item={item} />
+      <button className="icon red">
+        <EyeIcon />
+      </button>
+    </div>
+  );
+}
 
-function Character({item}) {
-  return <div className="list__item">
-    <img src={item.image} alt={item.name} />
-   <h3 className="name">
-    <span>{item.gender === "Male" ? "👱🏼‍♂️" : "👩🏻"}</span>
-    <span>{item.name}</span>
-   </h3>
-   <div className="list-item__info info">
-    <span className={`status ${item.status === "Dead" ? "red" : ""}`}></span>
-    <span> {item.status} </span>
-    <span> - {item.species}</span>
-   </div>
-   <button className="icon red">
-    <EyeIcon/>
-   </button>
-    </div>;
+function CharacterName({ item }) {
+  return (
+    <h3 className="name">
+      <span>{item.gender === "Male" ? "👱🏼‍♂️" : "👩🏻"}</span>
+      <span>{item.name}</span>
+    </h3>
+  );
+}
+
+function CharacterInfo({ item }) {
+  return (
+    <div className="list-item__info info">
+      <span className={`status ${item.status === "Dead" ? "red" : ""}`}></span>
+      <span> {item.status} </span>
+      <span> - {item.species}</span>
+    </div>
+  );
 }
