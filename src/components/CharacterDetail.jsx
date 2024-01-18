c6951367fd46f5e883f0db0890e200eaa047f636
mehrnaz <pishyar.mehrnaz@gmail.com>
2024-01-18 19:58:40 +0100

    add responsive

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -22,7 +22,7 @@ const CharacterDetail = ({ selectedId, onAddFavourites, isAddToFavourite }) => {
           `https://rickandmortyapi.com/api/episode/${episodeId}`
         );
         // console.log([episodeData])
-        setEpisodes([episodeData].flat());
+        setEpisodes([episodeData].flat().slice(0,5));
         setCharacter(data);
       } catch (error) {
         toast.error(error.response.data.error);
